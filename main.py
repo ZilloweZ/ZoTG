262400d0f519fb4a173c88de818597669dde44ec
ZilloweZ <110313697+ZilloweZ@users.noreply.github.com>
2023-03-08 15:19:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,18 @@
-from flask import Flask, request
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPTNeoForCausalLM, GPT2TokenizerFast
+from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-app = Flask(__name__)
+app = FastAPI()
 
-tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
-model_gpt2 = GPT2LMHeadModel.from_pretrained(
-    'gpt2', pad_token_id=tokenizer_gpt2.eos_token_id)
+class Message(BaseModel):
+    message: str
 
-tokenizer_gptneo = GPT2TokenizerFast.from_pretrained("EleutherAI/gpt-neo-1.3B")
-model_gptneo = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=tokenizer.eos_token_id)
 
-
-@app.route('/chatbot', methods=['POST'])
-def chatbot():
-    message = request.form['message']
-    input_ids_gpt2 = tokenizer_gpt2.encode(message, return_tensors='pt')
-    output_gpt2 = model_gpt2.generate(input_ids_gpt2,
-                                      max_length=1000,
-                                      do_sample=True)
-    response_gpt2 = tokenizer_gpt2.decode(output_gpt2[0],
-                                          skip_special_tokens=True)
-
-    input_ids_gptneo = tokenizer_gptneo.encode(message,
-                                               return_tensors='pt')
-    output_gptneo = model_gptneo.generate(input_ids_gptneo,
-                                          max_length=1000,
-                                          do_sample=True)
-    response_gptneo = tokenizer_gptneo.decode(output_gptneo[0],
-                                              skip_special_tokens=True)
-
-    response = f"GPT-2: {response_gpt2}\n\nGPT-Neo: {response_gptneo}"
-    return response
-
-
-if __name__ == '_main_':
-    app.run(host='0.0.0.0')
+@app.post('/chatbot')
+def chatbot(message: Message):
+    input_ids = tokenizer.encode(message.message, return_tensors='pt')
+    output = model.generate(input_ids, max_length=1000, do_sample=True)
+    response = tokenizer.decode(output[0], skip_special_tokens=True)
+    return {'response': response}
